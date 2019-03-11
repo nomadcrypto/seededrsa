8714289df1adb038ae8b70fd44e75e4e9e5fb618
= <nomadcrypto@gmail.com>
2019-03-11 13:24:13 -1000

    fixed indentation and typos

--- rng.js
+++ rng.js
@@ -1,76 +1,76 @@
 const bip39 = require("bip39");
 
 class RNG {
-	constructor(seed) {
-		if(!bip39.validateMnemonic(seed)) {
+    constructor(seed) {
+        if(!bip39.validateMnemonic(seed)) {
             throw new Error("Invalid Mnemonic phrase!");
         }
         seed = bip39.mnemonicToSeedHex(seed);
-		this.seed = seed;
-		this.rng = this.sfc32();
-	}
+        this.seed = seed;
+        this.rng = this.sfc32();
+    }
 
-	xmur3() {
-		for(var i = 0, h = 1779033703 ^ this.seed.length; i < this.seed.length; i++) {
-			h = Math.imul(h ^ this.seed.charCodeAt(i), 3432918353);
-			h = h << 13 | h >>> 19;
-		}
+    xmur3() {
+        for(var i = 0, h = 1779033703 ^ this.seed.length; i < this.seed.length; i++) {
+            h = Math.imul(h ^ this.seed.charCodeAt(i), 3432918353);
+            h = h << 13 | h >>> 19;
+        }
         
         
-	    return function() {
-	        h = Math.imul(h ^ h >>> 16, 2246822507);
-	        h = Math.imul(h ^ h >>> 13, 3266489909);
-	        return (h ^= h >>> 16) >>> 0;
-	    }		
-	}
+        return function() {
+            h = Math.imul(h ^ h >>> 16, 2246822507);
+            h = Math.imul(h ^ h >>> 13, 3266489909);
+            return (h ^= h >>> 16) >>> 0;
+        }       
+    }
 
-	sfc32() {
-		let seed = this.xmur3();
-		let a = seed();
-		let b = seed();
-		let c = seed();
-		let d = seed();
-	    return function() {
-	      a >>>= 0; b >>>= 0; c >>>= 0; d >>>= 0; 
-	      var t = (a + b) | 0;
-	      a = b ^ b >>> 9;
-	      b = c + (c << 3) | 0;
-	      c = (c << 21 | c >>> 11);
-	      d = d + 1 | 0;
-	      t = t + d | 0;
-	      c = c + t | 0;
-	      return (t >>> 0) / 4294967296;
-	    }		
-	}
+    sfc32() {
+        let seed = this.xmur3();
+        let a = seed();
+        let b = seed();
+        let c = seed();
+        let d = seed();
+        return function() {
+          a >>>= 0; b >>>= 0; c >>>= 0; d >>>= 0; 
+          var t = (a + b) | 0;
+          a = b ^ b >>> 9;
+          b = c + (c << 3) | 0;
+          c = (c << 21 | c >>> 11);
+          d = d + 1 | 0;
+          t = t + d | 0;
+          c = c + t | 0;
+          return (t >>> 0) / 4294967296;
+        }       
+    }
 
-	dec2Hex(dec) {
-		return ('0' + dec.toString(16)).substr(-2)
-	}
+    dec2Hex(dec) {
+        return ('0' + dec.toString(16)).substr(-2)
+    }
 
-	random() {
-		return this.rng();
-	}
+    random() {
+        return this.rng();
+    }
 
-	randomRange(min, max) {
-		return Math.floor(this.random() * (max - min + 1)) + min;
-	}
+    randomRange(min, max) {
+        return Math.floor(this.random() * (max - min + 1)) + min;
+    }
 
-	randomValues(array) {
-		for(var i=0;i<array.length;i++) {
-			array[i] = this.randomRange(0, 255);
-		}
-	}
+    randomValues(array) {
+        for(var i=0;i<array.length;i++) {
+            array[i] = this.randomRange(0, 255);
+        }
+    }
 
-	nextBytes(array) {
-		let arr = new Uint8Array(array.length / 2)
-		this.randomValues(arr);
-		let bytes = Array.from(arr, this.dec2hex).join("");
-		for(var i = 0; i < array.length; ++i) {
-        	array[i] = bytes.charCodeAt(i);
+    nextBytes(array) {
+        let arr = new Uint8Array(array.length / 2)
+        this.randomValues(arr);
+        let bytes = Array.from(arr, this.dec2hex).join("");
+        for(var i = 0; i < array.length; ++i) {
+            array[i] = bytes.charCodeAt(i);
         }
 
 
-	}
+    }
 }
 
 module.exports = RNG;
